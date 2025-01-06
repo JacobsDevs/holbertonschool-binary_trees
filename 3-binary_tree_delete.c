30463ac8e46013124ae1666e6ba75969ad5444fb
JacobsDevs <jacobdevsmail@gmail.com>
2025-01-06 13:42:34 +1100

    fix: 3* Betty Formatting

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -2,6 +2,10 @@
 #include <stddef.h>
 #include <stdlib.h>
 
+/**
+ * binary_tree_delete - Recursively deletes the tree
+ * @tree: Pointer to the head of the tree
+ */
 void binary_tree_delete(binary_tree_t *tree)
 {
 	if (tree == NULL)
