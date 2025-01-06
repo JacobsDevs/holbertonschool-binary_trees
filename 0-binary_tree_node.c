fcb24206929531d44462c3b0ee6bfaa3a6046347
JacobsDevs <jacobdevsmail@gmail.com>
2025-01-06 13:03:31 +1100

    Betty formatting for 0-binary_tree_node.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,18 +1,25 @@
 #include "binary_trees.h"
 #include <stdlib.h>
 
+/**
+ * binary_tree_node - Return a new binary_tree_node pointer
+ * @parent: Pointer to the parent. Leave NULL for head of tree.
+ * @value: The value of the binary_tree.
+ *
+ * Return: Pointer to the new binary_tree or NULL on fail
+ */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-        binary_tree_t *node = malloc(sizeof(binary_tree_t));
-        node->n = value;
-        node->left = NULL;
-        node->right = NULL;
+	binary_tree_t *node = malloc(sizeof(binary_tree_t));
 
-        if (node == NULL)
-                return (NULL);
-        if (parent)
-                node->parent = parent;
-        else
-                node->parent = NULL;
-        return (node);
+	node->n = value;
+	node->left = NULL;
+	node->right = NULL;
+	if (node == NULL)
+		return (NULL);
+	if (parent)
+		node->parent = parent;
+	else
+		node->parent = NULL;
+	return (node);
 }
--- binary_trees.h
+++ binary_trees.h
@@ -3,11 +3,22 @@
 
 #include <stddef.h>
 
-typedef struct binary_tree {
-        int n;
-        struct binary_tree *parent;
-        struct binary_tree *left;
-        struct binary_tree *right;
+/**
+ * struct binary_tree - Struct for binary_tree_node
+ * @n: The value stored in the binary_tree.
+ * @parent: The parent of this node.
+ * @left: The left child of this node.
+ * @right: The right child of this node.
+ *
+ * Description: Struct for the binary_tree_node that stores a value
+ * and two children.
+ */
+typedef struct binary_tree
+{
+	int n;
+	struct binary_tree *parent;
+	struct binary_tree *left;
+	struct binary_tree *right;
 
 } binary_tree_t;
 
