8ca00251210cafc5883a740f20b69961ba2a9de8
JacobsDevs <jacobdevsmail@gmail.com>
2025-01-06 14:09:22 +1100

    fix: Betty Formatting

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,6 +1,12 @@
 #include "binary_trees.h"
 #include <stddef.h>
 
+/**
+ * binary_tree_is_root - Returns if a node is the root or not.
+ * @node: Pointer to the node to check.
+ *
+ * Return: 1 if root, 0 if not root on NULL
+ */
 int binary_tree_is_root(const binary_tree_t *node)
 {
 	if (node == NULL)
