9b253508182210d2a7baf529bf963701f60cecdb
JacobsDevs <jacobdevsmail@gmail.com>
2025-01-06 12:48:48 +1100

    fix: 0-binary_tree_node if no parent, parent = NULL

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -12,5 +12,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
                 return (NULL);
         if (parent)
                 node->parent = parent;
+        else
+                node->parent = NULL;
         return (node);
 }
