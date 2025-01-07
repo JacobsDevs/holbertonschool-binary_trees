506aead45bffd96156498ddf5081a7ac02064a67
JacobsDevs <jacobdevsmail@gmail.com>
2025-01-07 14:24:39 +1100

    fix: 9-* Incorrect result for imperfect tree.

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -15,15 +15,14 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	int right_height = 0;
 
 	if (tree == NULL)
+		return (-1);
+	left_height += binary_tree_height(tree->left);
+	right_height += binary_tree_height(tree->right);
+	if (left_height == -1 && right_height == -1)
 		return (0);
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
-	if (tree->left != NULL)
-		return (left_height + 1);
-	if (tree->right != NULL)
-		return (right_height + 1);
 	if (left_height >= right_height)
-		return (left_height);
+		return (left_height + 1);
 	else
-		return (right_height);
+		return (right_height + 1);
+
 }
