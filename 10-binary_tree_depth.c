880faad812179aa4ed19f76a78c3566c681b8743
JacobsDevs <jacobdevsmail@gmail.com>
2025-01-07 14:39:23 +1100

    fix: 10-* Infinite loop on null tree

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -13,7 +13,7 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	int depth = 0;
 
-	if (tree->parent == NULL)
+	if (tree->parent == NULL || tree == NULL)
 		return (0);
 	depth = binary_tree_depth(tree->parent);
 	depth++;
