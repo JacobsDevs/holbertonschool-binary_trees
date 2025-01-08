0c886eb3059ad63000a151f9b2e7814b3167720a
JacobsDevs <jacobdevsmail@gmail.com>
2025-01-08 12:17:28 +1100

    fix: 17-* handle null node

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -12,7 +12,7 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
 	binary_tree_t *parent = NULL;
 
-	if (node->parent == NULL)
+	if (node == NULL || node->parent == NULL)
 		return (NULL);
 	parent = node->parent;
 	if (parent->left == NULL || parent->right == NULL)
